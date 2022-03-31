3238a7f2d41e79aa49ad536a7e0916cd120a00a8
Bhargav Borse <bhargavborse4898@gmail.com>
2022-03-31 13:47:38 +0530

    solved file deletion error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {XmlStructureComponent} from '../app/xml-structure/xml-structure.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'xml-structure', component: XmlStructureComponent }
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,10 @@ import { FabricjsEditorModule } from 'projects/angular-editor-fabric-js/src/publ
 import { FormsModule } from '@angular/forms';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { AppRoutingModule } from './app-routing.module';
-import { XmlStructureComponent } from './xml-structure/xml-structure.component';
-import { HomeComponent } from './home/home.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: AppComponent },
-  { path: 'xml-structure', component: XmlStructureComponent },
-  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '' }
   ];
 
@@ -23,8 +19,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    XmlStructureComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,7 +26,6 @@ const appRoutes: Routes = [
     FormsModule,
     ColorPickerModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
