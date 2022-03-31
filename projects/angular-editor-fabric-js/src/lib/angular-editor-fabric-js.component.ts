dc232344c0d669f3a2aad8b2a25597936466e662
Bhargav Borse <bhargavborse4898@gmail.com>
2022-03-31 13:42:42 +0530

    added XML Data

--- projects/angular-editor-fabric-js/src/lib/angular-editor-fabric-js.component.ts
+++ projects/angular-editor-fabric-js/src/lib/angular-editor-fabric-js.component.ts
@@ -42,6 +42,7 @@ export class FabricjsEditorComponent implements AfterViewInit {
 
   public json: any;
   public xml: any;
+  public xmlData : any;
   private globalEditor = false;
   public textEditor = false;
   private imageEditor = false;
@@ -655,15 +656,69 @@ export class FabricjsEditorComponent implements AfterViewInit {
 
     this.xml = JSON.stringify(this.canvas, null, 2);
     var json = JSON.parse(this.xml);
-    console.log(js2xmlparser.parse("xml", json));
-    const creatorTool = json.background;
+    // alert(json.objects[0].fill);
 
-    document.getElementById('creatorTool').innerHTML = creatorTool;
-    const data = "in xmlJson";
-    this.xml = data;
+    this.xmlData = `<?xml version="1.0" encoding="utf-8"?>
+    <x:xmpmeta xmlns:x="adobe:ns:meta/">
+        <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+            <rdf:Description rdf:about="" xmlns:xmp="http://ns.abobe.com/xap/1.0/">
+                <xmp:CreatorTool id="creatorTool">Atalasoft DotAnnotate 11.2</xmp:CreatorTool>
+                <xmp:CreateDate><![CDATA[2022-03-08T02:19:13]]></xmp:CreateDate>
+            </rdf:Description>
+            <rdf:Description rdf:about="DotAnnotate Annotations" xmlns:xmp="http://ns.abobe.com/xap/4.0/">
+                <DocumentResolution>1,1</DocumentResolution>
+                <LayerDataCollection>
+                    <LayerData assembly="Atalasoft.dotImage" namespace="Atalasoft.Annotate">
+                        <CreationTime type="System.String"><![CDATA[2022-03-08T02:19:12]]></CreationTime>
+                        <ModifiedTime type="System.String"><![CDATA[2022-03-08T02:19:12]]></ModifiedTime>
+                        <Location type="System.String"><![CDATA[359.0909,245.4545]]></Location>
+                        <Rotation type="System.Single">0</Rotation>
+                        <Size type="System.String"><![CDATA[404.5454,259.0909]]></Size>
+                        <Visible type="System.Boolean">True</Visible>
+                        <CanMove type="System.Boolean">True</CanMove>
+                        <CanResize type="System.Boolean">True</CanResize>
+                        <CanRotate type="System.Boolean">False</CanRotate>
+                        <CanMirror type="System.Boolean">False</CanMirror>
+                        <CanSelect type="System.Boolean">True</CanSelect>
+                        <ExtraProperties assembly="System" type="System.Collections.Specialized.StringCollection" />
+                        <GroupAnnotation type="System.Boolean">False</GroupAnnotation>
+                        <Items assembly="Atalasoft.dotImage" type="Atalasoft.Annotate.AnnotationDataCollection">
+                            <Items type="System.Collections.ArrayList">
+                                <RectangleData assembly="Atalasoft.dotImage" namespace="Atalasoft.Annotate">
+                                    <CreationTime type="System.String"><![CDATA[2022-03-08T02:19:11]]></CreationTime>
+                                    <ModifiedTime type="System.String"><![CDATA[2022-03-08T02:19:12]]></ModifiedTime>
+                                    <Location type="System.String"><![CDATA[359.0909,245.4545]]></Location>
+                                    <Rotation type="System.Single">0</Rotation>
+                                    <Size type="System.String"><![CDATA[404.5454,259.0909]]></Size>
+                                    <Visible type="System.Boolean">True</Visible>
+                                    <CanMove type="System.Boolean">True</CanMove>
+                                    <CanResize type="System.Boolean">True</CanResize>
+                                    <CanRotate type="System.Boolean">True</CanRotate>
+                                    <CanMirror type="System.Boolean">True</CanMirror>
+                                    <CanSelect type="System.Boolean">True</CanSelect>
+                                    <ExtraProperties assembly="System" type="System.Collections.Specialized.StringCollection" />
+                                    <Fill assembly="Atalasoft.dotImage" type="Atalasoft.Annotate.AnnotationBrush">
+                                        <ctor type="System.Int32">0</ctor>
+                                        <Color type="System.Int32">-16777216</Color>
+                                    </Fill>
+                                    <Translucent type="System.Boolean">False</Translucent>
+                                </RectangleData>
+                            </Items>
+                        </Items>
+                    </LayerData>
+                </LayerDataCollection>
+            </rdf:Description>
+        </rdf:RDF>
+        </x:xmpmeta>`; 
+        // alert(js2xmlparser.parse("xml", json.objects[0].fill));
+    // const creatorTool = json.background;
+
+    // document.getElementById('creatorTool').innerHTML = creatorTool;
+    // const data = "in xmlJson";
+    // this.xml = data;
     // var json = JSON.parse(this.json);
     // console.log(js2xmlparser.parse("xml", json));
-    // alert(json.objects[0].fill);
+    // console.log(json.objects[0].fill);
     // let obj = {
     //   "firstName": "John",
     //   "lastName": "Smith",
