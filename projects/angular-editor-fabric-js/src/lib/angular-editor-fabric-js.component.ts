22d30a4cd734c3aad6713ae54fbb200993ba1700
Bhargav Borse <bhargavborse4898@gmail.com>
2022-04-18 12:19:13 +0530

    atalasoft updated code for rect

--- projects/angular-editor-fabric-js/src/lib/angular-editor-fabric-js.component.ts
+++ projects/angular-editor-fabric-js/src/lib/angular-editor-fabric-js.component.ts
@@ -690,22 +690,62 @@ export class FabricjsEditorComponent implements AfterViewInit {
 
               this.xmlData += `
               <RectangleData assembly="Atalasoft.dotImage" namespace="Atalasoft.Annotate">
-              <CreationTime type="System.String"><![CDATA[2022-03-08T02:19:11]]></CreationTime>
-              <ModifiedTime type="System.String"><![CDATA[2022-03-08T02:19:12]]></ModifiedTime>
-              <Location type="System.String"><![CDATA[`+ json.objects[i].left +`, `+ json.objects[i].top +`]]></Location>
+              <CreationTime type="System.String">
+                  <![CDATA[2022-04-18T02:31:30]]>
+              </CreationTime>
+              <ModifiedTime type="System.String">
+                  <![CDATA[2022-04-18T02:32:07]]>
+              </ModifiedTime>
+              <Location type="System.String">
+                  <![CDATA[`+ json.objects[i].left +`, `+ json.objects[i].top +`]]>
+              </Location>
               <Rotation type="System.Single">0</Rotation>
-              <Size type="System.String"><![CDATA[`+ mergeX +`, `+ mergeY +`]]></Size>
+              <Size type="System.String">
+                  <![CDATA[`+ mergeX +`, `+ mergeY +`]]>
+              </Size>
               <Visible type="System.Boolean">True</Visible>
               <CanMove type="System.Boolean">True</CanMove>
               <CanResize type="System.Boolean">True</CanResize>
               <CanRotate type="System.Boolean">True</CanRotate>
               <CanMirror type="System.Boolean">True</CanMirror>
               <CanSelect type="System.Boolean">True</CanSelect>
-              <ExtraProperties assembly="System" type="System.Collections.Specialized.StringCollection" />
+              <ExtraProperties assembly="System"
+                  type="System.Collections.Specialized.StringCollection" />
               <Fill assembly="Atalasoft.dotImage" type="Atalasoft.Annotate.AnnotationBrush">
                   <ctor type="System.Int32">0</ctor>
-                  <Color type="System.Int32">-16777216</Color>
+                  <Color type="System.Int32">16777215</Color>
               </Fill>
+              <Outline assembly="Atalasoft.dotImage" type="Atalasoft.Annotate.AnnotationPen">
+                  <ctor type="System.Int32">3</ctor>
+                  <Alignment type="System.String">
+                      <![CDATA[Center]]>
+                  </Alignment>
+                  <Color type="System.Int32">-16777216</Color>
+                  <DashCap type="System.String">
+                      <![CDATA[Round]]>
+                  </DashCap>
+                  <DashOffset type="System.Single">0</DashOffset>
+                  <DashStyle type="System.String">
+                      <![CDATA[Solid]]>
+                  </DashStyle>
+                  <EndCap assembly="Atalasoft.dotImage" type="Atalasoft.Annotate.AnnotationLineCap">
+                      <Style type="System.String"><![CDATA[None]]></Style>
+                      <Size type="System.String">
+                          <![CDATA[15,15]]>
+                      </Size>
+                  </EndCap>
+                  <LineJoin type="System.String">
+                      <![CDATA[Round]]>
+                  </LineJoin>
+                  <MiterLimit type="System.Single">0</MiterLimit>
+                  <StartCap assembly="Atalasoft.dotImage" type="Atalasoft.Annotate.AnnotationLineCap">
+                      <Style type="System.String"><![CDATA[None]]></Style>
+                      <Size type="System.String">
+                          <![CDATA[15,15]]>
+                      </Size>
+                  </StartCap>
+                  <Width type="System.Single">4</Width>
+              </Outline>
               <Translucent type="System.Boolean">False</Translucent>
           </RectangleData>`;   
         }
